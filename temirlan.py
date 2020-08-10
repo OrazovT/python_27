206ade7fcfedde8f492f97f158d1a7c485320c57
OrazovT <69306671+OrazovT@users.noreply.github.com>
2020-08-10 16:57:10 +0600

    Update temirlan.py

--- temirlan.py
+++ temirlan.py
@@ -24,3 +24,33 @@ elif num1 <= num3 and num2 <= num3:
     print(num3)
 else:
     print ('else')
+
+if num = 1:
+    print('Большой палец')
+elif num = 2:
+    print('Указательный')
+elif num = 3:
+    print('Средний')
+elif num = 4:
+    print('Безымянный')
+else num = 5:
+    print('Мизинчик')
+
+if num1 % num2:
+    print('Делиться')
+Else num1 % num2:
+    print('Не делиться')
+
+If num1 >= 0 and num2 >= 0:
+    print(num1 + num2)
+else:
+    print('Условия не выполняются')
+
+if num1 % 5:
+    print(num1)
+if num2 % 5:
+    print(num2)
+if num3 % 5:
+    print(num3)
+
+if num1 % 2:
